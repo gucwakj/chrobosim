1a3233ed4207dae8281bbed1df7c0714a87d4fe0
Kevin Gucwa <gucwakj@gmail.com>
2015-04-23 10:00:08 -0700

    remove ID from recordDistanceBegin() and End()

--- include/linkbot.h
+++ include/linkbot.h
@@ -163,8 +163,8 @@ class DLLIMPORT CLinkbotI {
 		int recordAngles(double time[:], double angle1[:], double angle2[:], double angle3[:], int num, double seconds, ...);
 		int recordAnglesBegin(robotRecordData_t &time, robotRecordData_t &a1, robotRecordData_t &a2, robotRecordData_t &a3, double seconds, ...);
 		int recordAnglesEnd(int &num);
-		int recordDistanceBegin(int id, robotRecordData_t &time, robotRecordData_t &distance, double radius, double seconds, ...);
-		int recordDistanceEnd(int id, int &num);
+		int recordDistanceBegin(robotRecordData_t &time, robotRecordData_t &distance, double radius, double seconds);
+		int recordDistanceEnd(int &num);
 		int recordDistanceOffset(double distance);
 		int recordDistancesBegin(robotRecordData_t &time, robotRecordData_t &d1, robotRecordData_t &d2, robotRecordData_t &d3, double radius, double seconds, ...);
 		int recordDistancesEnd(int &num);
@@ -428,8 +428,8 @@ class DLLIMPORT CLinkbotL {
 		int recordAngles(double time[:], double angle1[:], double angle2[:], double angle3[:], int num, double seconds, ...);
 		int recordAnglesBegin(robotRecordData_t &time, robotRecordData_t &a1, robotRecordData_t &a2, robotRecordData_t &a3, double seconds, ...);
 		int recordAnglesEnd(int &num);
-		int recordDistanceBegin(int id, robotRecordData_t &time, robotRecordData_t &distance, double radius, double seconds, ...);
-		int recordDistanceEnd(int id, int &num);
+		int recordDistanceBegin(robotRecordData_t &time, robotRecordData_t &distance, double radius, double seconds);
+		int recordDistanceEnd(int &num);
 		int recordDistanceOffset(double distance);
 		int recordDistancesBegin(robotRecordData_t &time, robotRecordData_t &d1, robotRecordData_t &d2, robotRecordData_t &d3, double radius, double seconds, ...);
 		int recordDistancesEnd(int &num);
