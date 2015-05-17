8b5a533d2d50215c97bf3d20452b7b89d1b30b0f
Kevin Gucwa <gucwakj@gmail.com>
2015-05-17 13:07:59 -0700

    make full ch package (with bad links)

--- createpkgWin64.sh
+++ createpkgWin64.sh
@@ -26,22 +26,49 @@ cp $ROOT/include/mindstorms.h $PKGDIR/include
 cp $ROOT/include/robot.h $PKGDIR/include
 cp $ROOT/include/macros.h $PKGDIR/include
 mkdir $PKGDIR/bin
+cp $ROOT/../librs/build/bin/rs.dll $PKGDIR/bin
+cp $ROOT/../librs/build/bin/rsRobots.dll $PKGDIR/bin
+cp $ROOT/../librs/build/bin/rsScene.dll $PKGDIR/bin
+cp $ROOT/../librs/build/bin/rsSim.dll $PKGDIR/bin
+cp $ROOT/../librs/build/bin/rsCallback.dll $PKGDIR/bin
+cp $ROOT/../librs/build/bin/rsXML.dll $PKGDIR/bin
 cp $ROOT/../librs/deps/ode/lib/Release/ode.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/build/bin/osg100-osg.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/build/bin/osg100-osgDB.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/build/bin/osg100-osgFX.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/build/bin/osg100-osgGA.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/build/bin/osg100-osgShadow.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/build/bin/osg100-osgText.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/build/bin/osg100-osgUtil.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/build/bin/osg100-osgViewer.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/build/bin/ot13-OpenThreads.dll $PKGDIR/bin
+# cp $ROOT/../librs/deps/osg/build/bin/osg100-osg.dll $PKGDIR/bin
+# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgDB.dll $PKGDIR/bin
+# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgFX.dll $PKGDIR/bin
+# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgGA.dll $PKGDIR/bin
+# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgShadow.dll $PKGDIR/bin
+# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgText.dll $PKGDIR/bin
+# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgUtil.dll $PKGDIR/bin
+#cp $ROOT/../librs/deps/osg/build/bin/osg100-osgViewer.dll $PKGDIR/bin
+#cp $ROOT/../librs/deps/osg/build/bin/ot13-OpenThreads.dll $PKGDIR/bin
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osg.dll $PKGDIR/bin
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgDB.dll $PKGDIR/bin
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgFX.dll $PKGDIR/bin
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgGA.dll $PKGDIR/bin
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgShadow.dll $PKGDIR/bin
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgText.dll $PKGDIR/bin
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgUtil.dll $PKGDIR/bin
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgViewer.dll $PKGDIR/bin
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/ot20-OpenThreads.dll $PKGDIR/bin
 cp $ROOT/../librs/deps/osg/third_party/x64/bin/glut32.dll $PKGDIR/bin
 cp $ROOT/../librs/deps/osg/third_party/x64/bin/libpng13.dll $PKGDIR/bin
 cp $ROOT/../librs/deps/osg/third_party/x64/bin/zlib1.dll $PKGDIR/bin
-mkdir $PKGDIR/bin/osgPlugins-3.2.0
-cp $ROOT/../librs/deps/osg/build/bin/osgPlugins-3.2.0/osgdb_png.dll $PKGDIR/bin/osgPlugins-3.2.0
-cp $ROOT/../librs/deps/osg/build/bin/osgPlugins-3.2.0/osgdb_3ds.dll $PKGDIR/bin/osgPlugins-3.2.0
+cp $ROOT/../RoboSim/build/bin/robosimwidget.dll $PKGDIR/bin
+cp $ROOT/../RoboSim/build/bin/RoboSim.exe $PKGDIR/bin
+cp C:/Qt/5.4.1/5.4/msvc2013_64_opengl/bin/Qt5Core.dll $PKGDIR/bin
+cp C:/Qt/5.4.1/5.4/msvc2013_64_opengl/bin/Qt5Gui.dll $PKGDIR/bin
+cp C:/Qt/5.4.1/5.4/msvc2013_64_opengl/bin/Qt5OpenGL.dll $PKGDIR/bin
+cp C:/Qt/5.4.1/5.4/msvc2013_64_opengl/bin/Qt5Widgets.dll $PKGDIR/bin
+cp C:/Qt/5.4.1/5.4/msvc2013_64_opengl/bin/icudt53.dll $PKGDIR/bin
+cp C:/Qt/5.4.1/5.4/msvc2013_64_opengl/bin/icuin53.dll $PKGDIR/bin
+cp C:/Qt/5.4.1/5.4/msvc2013_64_opengl/bin/icuuc53.dll $PKGDIR/bin
+#mkdir $PKGDIR/bin/osgPlugins-3.2.0
+#cp $ROOT/../librs/deps/osg/build/bin/osgPlugins-3.2.0/osgdb_png.dll $PKGDIR/bin/osgPlugins-3.2.0
+#cp $ROOT/../librs/deps/osg/build/bin/osgPlugins-3.2.0/osgdb_3ds.dll $PKGDIR/bin/osgPlugins-3.2.0
+mkdir $PKGDIR/bin/osgPlugins-3.2.1
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osgPlugins-3.2.1/osgdb_png.dll $PKGDIR/bin/osgPlugins-3.2.1
+cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osgPlugins-3.2.1/osgdb_3ds.dll $PKGDIR/bin/osgPlugins-3.2.1
 #mkdir $PKGDIR/docs
 #cp ../../docs/robosim.pdf $PKGDIR/docs
 #cp ../../docs/images/robosim.png $PKGDIR/docs
@@ -50,7 +77,7 @@ cp -R $ROOT/../librs/resources/background $PKGDIR/data
 cp -R $ROOT/../librs/resources/linkbot $PKGDIR/data
 cp -R $ROOT/../librs/resources/mindstorms $PKGDIR/data
 
-#cp $ROOT/Makefile $PACKAGE-$VERSION
+cp $ROOT/Makefile $PACKAGE-$VERSION
 cp $ROOT/pkginstall.ch $PACKAGE-$VERSION
 zip -rq $PACKAGE-$VERSION.zip $PACKAGE-$VERSION
 echo Done Building $PACKAGE-$VERSION
