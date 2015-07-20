2ce6c3361615d0a50672803cc6c5cd762e36c730
Kevin Gucwa <gucwakj@gmail.com>
2015-07-20 10:56:50 -0700

    link against newest OSG

--- createpkgWin64.sh
+++ createpkgWin64.sh
@@ -33,28 +33,22 @@ cp $ROOT/../librs/build/bin/rsSim.dll $PKGDIR/bin
 cp $ROOT/../librs/build/bin/rsCallback.dll $PKGDIR/bin
 cp $ROOT/../librs/build/bin/rsXML.dll $PKGDIR/bin
 cp $ROOT/../librs/deps/ode/lib/Release/ode.dll $PKGDIR/bin
-# cp $ROOT/../librs/deps/osg/build/bin/osg100-osg.dll $PKGDIR/bin
-# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgDB.dll $PKGDIR/bin
-# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgFX.dll $PKGDIR/bin
-# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgGA.dll $PKGDIR/bin
-# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgShadow.dll $PKGDIR/bin
-# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgText.dll $PKGDIR/bin
-# cp $ROOT/../librs/deps/osg/build/bin/osg100-osgUtil.dll $PKGDIR/bin
-#cp $ROOT/../librs/deps/osg/build/bin/osg100-osgViewer.dll $PKGDIR/bin
-#cp $ROOT/../librs/deps/osg/build/bin/ot13-OpenThreads.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osg.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgDB.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgFX.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgGA.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgQt.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgShadow.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgText.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgUtil.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osg100-osgViewer.dll $PKGDIR/bin
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/ot20-OpenThreads.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/third_party/x64/bin/glut32.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/third_party/x64/bin/libpng13.dll $PKGDIR/bin
-cp $ROOT/../librs/deps/osg/third_party/x64/bin/zlib1.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osg100-osg.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osg100-osgDB.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osg100-osgFX.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osg100-osgGA.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osg100-osgQt.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osg100-osgShadow.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osg100-osgText.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osg100-osgUtil.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osg100-osgViewer.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/ot20-OpenThreads.dll $PKGDIR/bin
+mkdir $PKGDIR/bin/osgPlugins-3.2.1
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osgPlugins-3.2.1/osgdb_png.dll $PKGDIR/bin/osgPlugins-3.2.1
+cp $ROOT/../librs/deps/osg3.2.1/build/bin/osgPlugins-3.2.1/osgdb_3ds.dll $PKGDIR/bin/osgPlugins-3.2.1
+cp $ROOT/../librs/deps/osg3.2.1/third_party/x64/bin/glut32.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/third_party/x64/bin/libpng13.dll $PKGDIR/bin
+cp $ROOT/../librs/deps/osg3.2.1/third_party/x64/bin/zlib1.dll $PKGDIR/bin
 cp $ROOT/../RoboSim/build/bin/robosimwidget.dll $PKGDIR/bin
 cp $ROOT/../RoboSim/build/bin/RoboSim.exe $PKGDIR/bin
 cp $ROOT/../RoboSim/qt.conf $PKGDIR/bin
@@ -70,9 +64,6 @@ mkdir -p $PKGDIR/bin/plugins/platforms
 cp C:/Qt/5.4.1/5.4/msvc2013_64_opengl/plugins/platforms/qwindows.dll $PKGDIR/bin/plugins/platforms
 cp $PKGDIR/dl/Microsoft.VC120.CRT/msvcr120.dll $PKGDIR/bin
 cp $PKGDIR/dl/Microsoft.VC120.CRT/msvcp120.dll $PKGDIR/bin
-mkdir $PKGDIR/bin/osgPlugins-3.2.1
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osgPlugins-3.2.1/osgdb_png.dll $PKGDIR/bin/osgPlugins-3.2.1
-cp C:/Users/kgucwa/Downloads/OpenSceneGraph-3.2.1/build/bin/osgPlugins-3.2.1/osgdb_3ds.dll $PKGDIR/bin/osgPlugins-3.2.1
 #mkdir $PKGDIR/docs
 #cp ../../docs/robosim.pdf $PKGDIR/docs
 #cp ../../docs/images/robosim.png $PKGDIR/docs
